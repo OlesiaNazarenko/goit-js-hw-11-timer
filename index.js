5f9278708936a4aee3a4b61386087d3b7cdca453
Olesia Nazarenko <80903117+OlesiaNazarenko@users.noreply.github.com>
2022-04-26 22:53:04 +0300

    added stopwatch

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ class CountdownTimer {
     this.selector = selector;
     this.targetDate = targetDate;
     this.intervalId = null;
-}
+  }
 
   getRefs() {
     const container = document.querySelector(this.selector);
@@ -11,37 +11,41 @@ class CountdownTimer {
     const hours = container.querySelector('[data-value="hours"]');
     const mins = container.querySelector('[data-value="mins"]');
     const secs = container.querySelector('[data-value="secs"]');
-    return {days, hours, mins, secs}
-}
-  pad(value) {
-    return String(value).padStart(2, '0')
+    return { days, hours, mins, secs };
   }
-  
+  pad(value) {
+    return String(value).padStart(2, "0");
+  }
+
   updateDate({ days, hours, mins, secs }) {
-  const time = this.targetDate - Date.now();
- 
+    const time = this.targetDate - Date.now();
+
     if (this.targetDate <= Date.now()) {
-      clearInterval( this.intervalId);
-      days.textContent = '00';
-      hours.textContent = '00';
-      mins.textContent = '00';
-      secs.textContent = '00';
+      clearInterval(this.intervalId);
+      days.textContent = "00";
+      hours.textContent = "00";
+      mins.textContent = "00";
+      secs.textContent = "00";
     } else {
-        days.textContent = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
-        hours.textContent = this.pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
-        mins.textContent = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
-        secs.textContent = this.pad(Math.floor((time % (1000 * 60)) / 1000));
+      days.textContent = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+      hours.textContent = this.pad(
+        Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+      );
+      mins.textContent = this.pad(
+        Math.floor((time % (1000 * 60 * 60)) / (1000 * 60))
+      );
+      secs.textContent = this.pad(Math.floor((time % (1000 * 60)) / 1000));
     }
   }
   interval() {
-    this.intervalId = setInterval(()=> {
-      this.updateDate(this.getRefs())
-      }, 1000);
- }  
+    this.intervalId = setInterval(() => {
+      this.updateDate(this.getRefs());
+    }, 1000);
+  }
 }
 
 const timer = new CountdownTimer({
-  selector: '#timer-1',
-  targetDate: new Date('dec 13, 2021 22:22:00'),
+  selector: "#timer-1",
+  targetDate: new Date("sep 13, 2022 22:22:00"),
 });
 timer.interval();
---
+++ stopwatch.js
@@ -0,0 +1,46 @@
+class CountdownTimer {
+  constructor({ selector, targetDate }) {
+    this.selector = selector;
+    this.targetDate = targetDate;
+    this.intervalId = null;
+  }
+
+  getRefs() {
+    const container = document.querySelector(this.selector);
+    const month = container.querySelector(' [data-value="month"]');
+    const days = container.querySelector(' [data-value="days"]');
+    const hours = container.querySelector('[data-value="hours"]');
+    const mins = container.querySelector('[data-value="mins"]');
+    const secs = container.querySelector('[data-value="secs"]');
+    return { month, days, hours, mins, secs };
+  }
+  pad(value) {
+    return String(value).padStart(2, "0");
+  }
+
+  updateDate({ month, days, hours, mins, secs }) {
+    const time = new Date();
+    month.textContent = time.getMonth() - this.targetDate.getMonth();
+    days.textContent = this.pad(
+      this.pad(time.getDate() - this.targetDate.getDate())
+    );
+
+    hours.textContent = this.pad(time.getHours() - this.targetDate.getHours());
+    mins.textContent = this.pad(
+      time.getMinutes() - this.targetDate.getMinutes()
+    );
+    secs.textContent = this.pad(Math.floor((time % (1000 * 60)) / 1000));
+  }
+
+  interval() {
+    this.intervalId = setInterval(() => {
+      this.updateDate(this.getRefs());
+    }, 1000);
+  }
+}
+
+const timer = new CountdownTimer({
+  selector: "#timer-2",
+  targetDate: new Date("feb 24, 2022 03:40:00"),
+});
+timer.interval();
